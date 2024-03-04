2631583001e8228bd248899d3ca4bd229a78baab
subhrachk <subhra_chk@hotmail.co.uk>
2024-03-04 18:56:08 +0000

    commit04022024_1856

--- App.js
+++ App.js
@@ -2,27 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { useEffect, useState } from 'react';
 
-
-
-export default function App() {
-
-  const [postList,setpostList] = useState([]);
-  
-  const fetchData = async(_limit=10) => {
-    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=${_limit}`);
-    const data = await response.json();
-    setpostList(data);
-    console.log(data);
-    }
-
-  useEffect(()=>{fetchData()},[]);
-
-  
+export default function App() {  
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.listContainer}>
-        {postList.map(p => <Text>{p.id}</Text>)}
-      </View>
+      <Text>Stylesheet API</Text>
     </SafeAreaView>
   );
 }
@@ -30,10 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f5f5',
-    paddingTop: StatusBar.currebtHeight,
+    backgroundColor: 'plum',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  listContainer : {
-
-  }
 });
