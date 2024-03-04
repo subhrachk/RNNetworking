0a500ce41f865ec7fd07fa14f9fdb486d2002c79
subhrachk <subhra_chk@hotmail.co.uk>
2024-03-04 19:45:00 +0000

    commit04022024_1856

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { useEffect, useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
 
-export default function App() {  
+export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Stylesheet API</Text>
+      <View>
+        <Text style={[styles.box,styles.lightBlueBox,styles.boxShadow]}>LightBlue Box</Text>
+      </View>
+      <View>
+        <Text style={[styles.box,styles.lightGreenBox,,styles.boxShadow]}>LightGreen Box</Text>
+      </View>
     </SafeAreaView>
   );
 }
@@ -13,8 +18,32 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'plum',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "plum",
+    //padding: 60,
+    borderRadius: 8,
+    alignItems: "center",
+    //justifyContent: "center",
   },
+  box : {
+    width: 200,
+    height: 200,
+    borderWidth: 2,
+    borderColor: "red",
+    marginVertical: 10,
+    marginHorizontal: 10,
+    paddingVertical: 10,
+    paddingHorizontal : 20,
+  },
+  lightBlueBox: {
+    backgroundColor: "lightblue",
+  },
+  lightGreenBox: {
+    backgroundColor: "lightgreen",
+  },
+  boxShadow : {
+    shadowColor: "black",
+    shadowOffset : {width: 6, height: 6},
+    shadowOpacity: .5,
+    shadowRadius: 4
+  }
 });
